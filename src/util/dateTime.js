987b09184e2017fe659955a6080a4daef414bacd
Samuel <frontendsam@gmail.com>
2022-05-09 16:17:01 -0300

    remove dayjs

--- src/util/dateTime.js
+++ src/util/dateTime.js
@@ -1,19 +1,7 @@
-import dayjs from 'dayjs/esm/index'
-import * as utc from 'dayjs/plugin/utc'
-import * as timezone from 'dayjs/plugin/timezone'
-import 'dayjs/locale/en'
-
-dayjs.locale('en')
-dayjs.extend(utc)
-dayjs.extend(timezone)
-
 class DateTime {
-    constructor() {
-        this.zone = dayjs.tz.guess()
-    }
-
-    format(date, format = 'MM/DD/YYYY') {
-        return dayjs(date).format(format)
+    format(date) {
+        const newDate  = new Date(date)
+        return newDate.toLocaleDateString('en-US')
     }
 }
 
